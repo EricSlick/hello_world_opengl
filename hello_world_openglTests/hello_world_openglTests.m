e8fa9c2e2789e291b2e67a7331d634c10857147d
Eric Slick <eric@slickfamily.net>
2014-04-28 13:45:46 -0700

    Initial Commit

---
+++ hello_world_openglTests/hello_world_openglTests.m
@@ -0,0 +1,34 @@
+//
+//  hello_world_openglTests.m
+//  hello_world_openglTests
+//
+//  Created by ERIC SLICK on 4/28/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hello_world_openglTests : XCTestCase
+
+@end
+
+@implementation hello_world_openglTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
